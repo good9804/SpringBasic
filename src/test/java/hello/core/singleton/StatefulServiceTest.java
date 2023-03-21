0eb01a95e8285b7a3c1486f606ee5901d267f167
good9804 <ggoggoni98@gmail.com>
2023-03-21 17:33:59 +0900

    ~생성자 자동주입

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1=ac.getBean(StatefulService.class);
+        StatefulService statefulService2=ac.getBean(StatefulService.class);
+        int userAPrice=statefulService1.order("userA",10000);
+        int userBPrice=statefulService2.order("userB",20000);
+
+        System.out.println("price" + userAPrice);
+    }
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
